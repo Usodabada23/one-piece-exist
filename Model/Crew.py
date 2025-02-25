95bf89640bab942b8e5c2db677bf00222c181a1f
Usodabada23 <redaba6200@gmail.com>
2025-02-25 11:46:41 +0100

    MVC- Model Crew and pirates update

---
+++ Model/Crew.py
@@ -0,0 +1,6 @@
+class Crew:
+    def __init__(self,name:str,pirates:list,description:str,is_yonko:bool):
+        self.name = name
+        self.pirates = pirates
+        self.description = description
+        self.is_yonko = is_yonko
--- Model/Pirate.py
+++ Model/Pirate.py
@@ -1,6 +1,6 @@
 import DevilFruit
 class Pirate:
-    def __init__(self,name:str,age:int,height:int,birthDate:str,bounty:int,weapon:str,devilFruit:DevilFruit):
+    def __init__(self,name:str,age:int,height:int,birthDate:str,bounty:int,weapon:str,devilFruit:DevilFruit,was_supernova:bool):
         self.__name = name
         self.__age = age
         self.__height = height
@@ -8,6 +8,7 @@ class Pirate:
         self.__bounty = bounty
         self.__weapon = weapon
         self.__devilFruit = devilFruit
+        self.__was_supernova = was_supernova
 
 
     
