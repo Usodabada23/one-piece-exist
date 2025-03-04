9d234495802126e99701dde06a86ce9cdfaa46ab
Usodabada23 <redaba6200@gmail.com>
2025-03-04 12:29:13 +0100

    Routes- Marines and marine by id

--- Model/Marine.py
+++ Model/Marine.py
@@ -1,4 +1,5 @@
-import RankMarine
+from Model.RankMarine import RankMarine
+from Model.Database import Database
 class Marine:
     def __init__(self,name:str,age:int,height:int,cgbounty:str,weapon:str,devilFruit_id:int,rank:RankMarine):
         self.__name = name
@@ -8,7 +9,35 @@ class Marine:
         self.__weapon = weapon
         self.__devilFruit_id= devilFruit_id
         self.__rank = rank
-
+    
+    @staticmethod
+    def allMarines():
+        db = Database()
+        conn = db.getConnection()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                query = "SELECT * FROM marines;"
+                cursor.execute(query)
+                marines = cursor.fetchall()
+                return marines
+            except Exception as e:
+                print(f"❌ Error fetching marines info : {e}")
+                return None
+    @staticmethod
+    def marineById(id:int):
+        db = Database()
+        conn = db.getConnection()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                query = """SELECT * FROM marines WHERE id = %s;"""
+                cursor.execute(query,(id,))
+                marine = cursor.fetchone()
+                return marine
+            except Exception as e:
+                print(f"❌ Error to fetch marine with id : {id} : {e}")
+                return None
 
 
     
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from Model.Database import Database
 from Model.Pirate import Pirate
+from Model.Marine import Marine
 from flask import jsonify
 app = Flask(__name__)
 @app.route("/")
@@ -9,6 +10,7 @@ def hello_world():
     db.create_tables()  
     return "Hello, World! The One piece exist!"
 
+# Pirates routes
 @app.route("/pirates",methods=['GET'])
 def showAllPirates():
     pirates = Pirate.allPirates()
@@ -19,6 +21,17 @@ def showPirateById(id):
     pirate = Pirate.pirateById(id=id)
     return jsonify(pirate)
 
+# Marines routes
+
+@app.route("/marines",methods=['GET'])
+def showAllMarines():
+    marines = Marine.allMarines()
+    return jsonify(marines)
+
+@app.route("/marine/<int:id>", methods=['GET'])
+def showMarineById(id):
+    marine = Marine.marineById(id=id)
+    return jsonify(marine)
 
 if __name__ == "__main__":
     app.run(debug=True)
