5a7fb8808e44d6f53d02884dac54606704ae9738
Usodabada23 <redaba6200@gmail.com>
2025-03-06 09:29:15 +0100

    Route- Add island

--- routes/islands_routes.py
+++ routes/islands_routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, Blueprint,jsonify
+from flask import Flask, Blueprint,jsonify,request
 from Model.Island import Island
-
+import datetime
 islands_routes = Blueprint("islands_routes",__name__)
 
 # Islands routes
@@ -14,3 +14,35 @@ def showAllIslands():
 def showIslandById(id):
     island = Island.islandById(id=id)
     return jsonify(island)
+@islands_routes.route("/island/add", methods=['POST'])
+def addIsland():
+    try:
+
+        data = request.get_json()
+
+        required_fields = ["name", "location", "government"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"error": f"The field '{field}' is missing."}), 400
+
+        
+        affiliated_group = data.get("affiliated_group", None)
+        if affiliated_group is not None:
+            affiliated_group = int(affiliated_group)
+
+        cgbounty = data.get("cgbounty",None)
+        if cgbounty is not None:
+            cgbounty = int(cgbounty)
+
+
+        newIsland = Island(name=data["name"],location=data["location"],government=data["government"],affiliated_group=affiliated_group)
+
+        newIsland.add()
+
+        return jsonify({"message": "✅ Island added successfully!"}), 201
+
+    except ValueError as ve:
+        return jsonify({"error": f"Format error: {str(ve)}"}), 400
+
+    except Exception as e:
+        return jsonify({"error": f"Error adding pirate: {str(e)}"}), 500
